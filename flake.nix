da50acdf0ea484c5e5d970167dcc672f6eae114f
John Reed <reallyjohnreed+git@gmail.com>
2024-10-17 20:15:22 -0400

    initialize git repo and some files

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A simple flake that provides a development environment for Shawnee's ETEC 1811 course.";
+
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+        ];
+
+        packages = with pkgs; [
+          # libclang # i give up on clang for c and clangd, i'll just rough it out with no lsp, not like it oughta matter that much anyway
+          gcc
+        ];
+      };
+    }
+  );
+}
---
+++ make.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# compile binary
+test=$(gcc -Wall -Werror src/main.c -o ./out/bin)
+if [ $PIPESTATUS -eq 1 ]; then
+  printf "\r\nmake.sh: encountered compile error, exiting early\r\n"
+  exit
+fi
+
+# handle arguments
+for arg; do
+  case $arg in
+    run) # run the binary after compiling, passing on arguments
+      runargs="$@"
+      #runargs=$(echo "$runargs" | cut -c 5-)
+      runargs=${runargs#*run} # everything after "run" is an argument for the binary
+      printf "make.sh: running \"./bin$runargs\":\r\n\r\n" # run binary
+      ./bin ${runargs}
+      break # stop reading arguments after running
+      ;;
+    *)
+      printf "make.sh: hmm... what's that? I couldn't hear you (argument: $arg)\r\n"
+      ;;
+  esac
+done
